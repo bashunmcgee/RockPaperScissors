727431352032ca7cf8fe9ada26d039a2ea4d12a2
bashunmcgee <bashunmcgee93@gmail.com>
2023-01-22 12:50:51 -0700

    Update RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -37,6 +37,7 @@ let computerChoice = computerPlay();
 
 let scoreCard = ""
 
+//Player Winning Outcomes
   if((playerChoice  == 'rock' && computerChoice == 'scissors')||
       (playerChoice == 'scissors' && computerChoice == 'paper')||
       (playerChoice == 'paper' && computerChoice == 'rock'))
@@ -45,13 +46,14 @@ let scoreCard = ""
     scoreCard = (" You Win Congrats! " + playerChoice + " Beats " + computerChoice + " everytime! " + "<br>"
     + "<br> Computer Score : " + computerScore + "<br> Player Score : " + playerScore);
 
-
+//Gameover Stopping point
     if (playerScore == 7) {
       scoreCard += '<br><br>You won the game! Reload the page to play again'
         disableButtons()
     }
 
 }
+//PC Winning Options
 else if ((playerChoice == 'rock' && computerChoice  == 'paper') ||
         (playerChoice == 'scissors' && computerChoice  == 'rock') ||
         (playerChoice == 'paper' && computerChoice == 'scissors')){
@@ -60,7 +62,7 @@ else if ((playerChoice == 'rock' && computerChoice  == 'paper') ||
 
 
           scoreCard = ("<br><br> You Lose! " + computerChoice +  " Beats "+ playerChoice + " <br>Computer Score : " + computerScore + " <br>Player Score : " + playerScore );
-
+//Stoppping Point PC Wins
           if(computerScore == 7){
             scoreCard += "<br> <br>Bots Rule The World Lets Play Again!"
             disableButtons()
@@ -70,7 +72,7 @@ else if ((playerChoice == 'rock' && computerChoice  == 'paper') ||
 
 else {
 
-
+//Tie Outcome
   scoreCard = ( "<br>Computer and Human have Chose The same option? Its a Draw. <br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
 
 
